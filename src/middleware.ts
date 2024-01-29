807958286219723d3d2f254ae0711b60ceff2055
nghiavohuynhdai <nghiavo.developer@gmail.com>
2024-01-29 22:17:54 +0700

    update cookie lifetime and middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,7 @@
-import { cookies } from 'next/headers'
-import { NextResponse, type NextRequest } from 'next/server'
-
-export function middleware(request: NextRequest) {
-  const token = cookies().get('accessToken')
-
-  if (token) {
-    return NextResponse.redirect(request.nextUrl.origin)
-  }
+export function middleware() {
+  return
 }
 
 export const config = {
-  matcher: '/login-register'
+  matcher: '/:path*'
 }
