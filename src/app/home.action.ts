59511facff88397fb45d8c87d13427cebbedf9ab
ddwcsanh <ledoducanh03@gmail.com>
2024-02-04 20:53:08 +0700

    done api

--- src/app/home.action.ts
+++ src/app/home.action.ts
@@ -2,10 +2,22 @@
 
 import { callApi } from './actions'
 
-const ROOT_ENDPOINT = '/categories/customer'
+const ROOT_ENDPOINT_CATEGORIES = '/categories/customer'
+const ROOT_ENDPOINT_PRODUCT = '/products/public'
 
 export const getCategories = async (page: number, limit: number, sort: string) => {
-  const endpoint = `${ROOT_ENDPOINT}?page=${page}&limit=${limit}&sort=${sort}`
+  const endpoint = `${ROOT_ENDPOINT_CATEGORIES}?page=${page}&limit=${limit}&sort=${sort}`
+  try {
+    const response = await callApi('get', endpoint)
+
+    return response.data
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const getProductList = async (page: number, limit: number, sort: string) => {
+  const endpoint = `${ROOT_ENDPOINT_PRODUCT}?page=${page}&limit=${limit}&sort=${sort}`
   try {
     const response = await callApi('get', endpoint)
 
