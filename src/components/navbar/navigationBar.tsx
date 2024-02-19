73a155a3f223dac1720b9eea182cc417232ccf8b
nghiavohuynhdai <nghiavo.developer@gmail.com>
2024-02-19 16:21:55 +0700

    [Fix][Deepsource] Fix deepsource code review

--- src/components/navbar/navigationBar.tsx
+++ src/components/navbar/navigationBar.tsx
@@ -40,174 +40,172 @@ const NavigationBar = ({ isLogin, me }: NavigationBarProps) => {
   }
 
   return (
-    <>
-      {!(activePathname.includes('order') || activePathname.includes('login')) && (
-        <>
-          <Navbar
-            isBlurred={false}
-            maxWidth='xl'
-            classNames={{ wrapper: 'gap-8' }}
-            className='h-[72px] sm:h-24 static sm:sticky sm:border-b sm:border-default-100'
-          >
-            <NavbarContent justify='start' className='max-w-fit'>
-              <NavbarBrand>
-                <Link href='/'>
-                  <Image removeWrapper radius='none' src='/logo.svg' alt='logo' className='!h-10' />
-                </Link>
-              </NavbarBrand>
-            </NavbarContent>
-            <NavbarContent className='hidden sm:flex gap-8' justify='start'>
-              <NavbarItem>
-                <Link
-                  underline={activePathname === '/' ? 'always' : 'hover'}
-                  href='/'
-                  className={`font-medium underline-offset-8 decoration-2 ${activePathname === '/' ? 'text-[var(--primary-orange-text-color)]' : 'text-black'}`}
+    !(activePathname.includes('order') || activePathname.includes('login')) && (
+      <>
+        <Navbar
+          isBlurred={false}
+          maxWidth='xl'
+          classNames={{ wrapper: 'gap-8' }}
+          className='h-[72px] sm:h-24 static sm:sticky sm:border-b sm:border-default-100'
+        >
+          <NavbarContent justify='start' className='max-w-fit'>
+            <NavbarBrand>
+              <Link href='/'>
+                <Image removeWrapper radius='none' src='/logo.svg' alt='logo' className='!h-10' />
+              </Link>
+            </NavbarBrand>
+          </NavbarContent>
+          <NavbarContent className='hidden sm:flex gap-8' justify='start'>
+            <NavbarItem>
+              <Link
+                underline={activePathname === '/' ? 'always' : 'hover'}
+                href='/'
+                className={`font-medium underline-offset-8 decoration-2 ${activePathname === '/' ? 'text-[var(--primary-orange-text-color)]' : 'text-black'}`}
+              >
+                Trang chủ
+              </Link>
+            </NavbarItem>
+            <NavbarItem>
+              <Link
+                underline={activePathname === '/products' ? 'always' : 'hover'}
+                href='/'
+                className={`font-medium underline-offset-8 decoration-2 ${activePathname === '/products' ? 'text-[var(--primary-orange-text-color)]' : 'text-black'}`}
+              >
+                Sản phẩm
+              </Link>
+            </NavbarItem>
+            <NavbarItem>
+              <Link
+                underline={activePathname === '/contact' ? 'always' : 'hover'}
+                href='/'
+                className={`font-medium underline-offset-8 decoration-2 ${activePathname === '/contact' ? 'text-[var(--primary-orange-text-color)]' : 'text-black'}`}
+              >
+                Liên hệ
+              </Link>
+            </NavbarItem>
+          </NavbarContent>
+          <NavbarContent as='div' className='items-center gap-2' justify='end'>
+            <Input
+              isClearable
+              variant='bordered'
+              classNames={{
+                base: `${searchValue ? 'max-w-full' : 'max-w-12'}`,
+                mainWrapper: 'h-full',
+                input: 'text-small !pl-3 !pr-0 !mr-6',
+                inputWrapper: `h-full text-center font-normal text-default-foreground shadow-none focus-within:rounded-lg focus-within:hover:!border-black focus-within:hover:bg-white ${searchValue ? 'rounded-lg' : 'rounded-full border-white hover:!border-default/5 hover:bg-default/30'}`
+              }}
+              placeholder='Tìm kiếm...'
+              value={searchValue}
+              onValueChange={(value: string) => setSearchValue(value)}
+              onClear={() => setSearchValue('')}
+              size='lg'
+              startContent={<FaSearch className='min-w-5 min-h-5 pointer-events-none' />}
+              className='focus-within:max-w-full transition-all !duration-300 !ease-linear hidden sm:flex'
+            />
+            <Button
+              as={Link}
+              href={isLogin ? '/cart' : '/login-register'}
+              size='lg'
+              isIconOnly
+              variant='light'
+              radius='full'
+              className={`hidden sm:flex ${activePathname === '/cart' ? 'text-[var(--primary-orange-text-color)]' : ''}`}
+            >
+              <FaShoppingCart className='min-w-5 min-h-5' />
+            </Button>
+            {isLogin && me ? (
+              <Dropdown placement='bottom-end'>
+                <DropdownTrigger>
+                  <Button size='lg' isIconOnly variant='light' radius='full'>
+                    <FaUser className='min-w-5 min-h-5' />
+                  </Button>
+                </DropdownTrigger>
+                <DropdownMenu aria-label='Profile Actions' variant='flat' disabledKeys={['profile']}>
+                  <DropdownItem isReadOnly textValue='profile' key='profile' className='opacity-100'>
+                    <p className='font-semibold'>{me.email}</p>
+                  </DropdownItem>
+                  <DropdownItem textValue='logout' key='logout' color='danger' onClick={logoutAction}>
+                    Đăng xuất
+                  </DropdownItem>
+                </DropdownMenu>
+              </Dropdown>
+            ) : (
+              <>
+                <Button
+                  as={Link}
+                  variant='light'
+                  href='/login-register'
+                  size='lg'
+                  isIconOnly
+                  radius='full'
+                  className='sm:hidden min-w-fit'
                 >
-                  Trang chủ
-                </Link>
-              </NavbarItem>
-              <NavbarItem>
-                <Link
-                  underline={activePathname === '/products' ? 'always' : 'hover'}
-                  href='/'
-                  className={`font-medium underline-offset-8 decoration-2 ${activePathname === '/products' ? 'text-[var(--primary-orange-text-color)]' : 'text-black'}`}
+                  <FaArrowRightToBracket className='min-w-5 min-h-5' />
+                </Button>
+                <Button
+                  as={Link}
+                  href='/login-register'
+                  className='hidden sm:flex min-w-fit bg-black text-white font-medium'
                 >
-                  Sản phẩm
-                </Link>
-              </NavbarItem>
-              <NavbarItem>
-                <Link
-                  underline={activePathname === '/contact' ? 'always' : 'hover'}
-                  href='/'
-                  className={`font-medium underline-offset-8 decoration-2 ${activePathname === '/contact' ? 'text-[var(--primary-orange-text-color)]' : 'text-black'}`}
-                >
-                  Liên hệ
-                </Link>
-              </NavbarItem>
-            </NavbarContent>
-            <NavbarContent as='div' className='items-center gap-2' justify='end'>
-              <Input
-                isClearable
-                variant='bordered'
-                classNames={{
-                  base: `${searchValue ? 'max-w-full' : 'max-w-12'}`,
-                  mainWrapper: 'h-full',
-                  input: 'text-small !pl-3 !pr-0 !mr-6',
-                  inputWrapper: `h-full text-center font-normal text-default-foreground shadow-none focus-within:rounded-lg focus-within:hover:!border-black focus-within:hover:bg-white ${searchValue ? 'rounded-lg' : 'rounded-full border-white hover:!border-default/5 hover:bg-default/30'}`
-                }}
-                placeholder='Tìm kiếm...'
-                value={searchValue}
-                onValueChange={(value: string) => setSearchValue(value)}
-                onClear={() => setSearchValue('')}
-                size='lg'
-                startContent={<FaSearch className='min-w-5 min-h-5 pointer-events-none' />}
-                className='focus-within:max-w-full transition-all !duration-300 !ease-linear hidden sm:flex'
-              />
+                  Đăng nhập
+                </Button>
+              </>
+            )}
+          </NavbarContent>
+        </Navbar>
+
+        {/* <ProgressLoading /> */}
+
+        <Navbar
+          isBlurred={false}
+          maxWidth='xl'
+          classNames={{ wrapper: 'justify-center' }}
+          className='h-[72px] top-[calc(100%-72px)] fixed sm:hidden border-t border-default-100'
+        >
+          <NavbarContent className='flex gap-8 !justify-evenly'>
+            <NavbarItem>
               <Button
-                as={Link}
-                href={isLogin ? '/cart' : '/login-register'}
-                size='lg'
                 isIconOnly
                 variant='light'
+                size='lg'
                 radius='full'
-                className={`hidden sm:flex ${activePathname === '/cart' ? 'text-[var(--primary-orange-text-color)]' : ''}`}
+                as={Link}
+                href='/'
+                className={`${activePathname === '/' ? 'text-[var(--primary-orange-text-color)]' : ''}`}
+              >
+                <FaHome className='min-w-5 min-h-5' />
+              </Button>
+            </NavbarItem>
+            <NavbarItem>
+              <Button
+                isIconOnly
+                variant='light'
+                size='lg'
+                radius='full'
+                as={Link}
+                href='/'
+                className={`${activePathname === '/search' ? 'text-[var(--primary-orange-text-color)]' : ''}`}
+              >
+                <FaSearch className='min-w-5 min-h-5' />
+              </Button>
+            </NavbarItem>
+            <NavbarItem>
+              <Button
+                isIconOnly
+                variant='light'
+                size='lg'
+                radius='full'
+                as={Link}
+                href='/cart'
+                className={`${activePathname === '/cart' ? 'text-[var(--primary-orange-text-color)]' : ''}`}
               >
                 <FaShoppingCart className='min-w-5 min-h-5' />
               </Button>
-              {isLogin && me ? (
-                <Dropdown placement='bottom-end'>
-                  <DropdownTrigger>
-                    <Button size='lg' isIconOnly variant='light' radius='full'>
-                      <FaUser className='min-w-5 min-h-5' />
-                    </Button>
-                  </DropdownTrigger>
-                  <DropdownMenu aria-label='Profile Actions' variant='flat' disabledKeys={['profile']}>
-                    <DropdownItem isReadOnly textValue='profile' key='profile' className='opacity-100'>
-                      <p className='font-semibold'>{me.email}</p>
-                    </DropdownItem>
-                    <DropdownItem textValue='logout' key='logout' color='danger' onClick={logoutAction}>
-                      Đăng xuất
-                    </DropdownItem>
-                  </DropdownMenu>
-                </Dropdown>
-              ) : (
-                <>
-                  <Button
-                    as={Link}
-                    variant='light'
-                    href='/login-register'
-                    size='lg'
-                    isIconOnly
-                    radius='full'
-                    className='sm:hidden min-w-fit'
-                  >
-                    <FaArrowRightToBracket className='min-w-5 min-h-5' />
-                  </Button>
-                  <Button
-                    as={Link}
-                    href='/login-register'
-                    className='hidden sm:flex min-w-fit bg-black text-white font-medium'
-                  >
-                    Đăng nhập
-                  </Button>
-                </>
-              )}
-            </NavbarContent>
-          </Navbar>
-
-          {/* <ProgressLoading /> */}
-
-          <Navbar
-            isBlurred={false}
-            maxWidth='xl'
-            classNames={{ wrapper: 'justify-center' }}
-            className='h-[72px] top-[calc(100%-72px)] fixed sm:hidden border-t border-default-100'
-          >
-            <NavbarContent className='flex gap-8 !justify-evenly'>
-              <NavbarItem>
-                <Button
-                  isIconOnly
-                  variant='light'
-                  size='lg'
-                  radius='full'
-                  as={Link}
-                  href='/'
-                  className={`${activePathname === '/' ? 'text-[var(--primary-orange-text-color)]' : ''}`}
-                >
-                  <FaHome className='min-w-5 min-h-5' />
-                </Button>
-              </NavbarItem>
-              <NavbarItem>
-                <Button
-                  isIconOnly
-                  variant='light'
-                  size='lg'
-                  radius='full'
-                  as={Link}
-                  href='/'
-                  className={`${activePathname === '/search' ? 'text-[var(--primary-orange-text-color)]' : ''}`}
-                >
-                  <FaSearch className='min-w-5 min-h-5' />
-                </Button>
-              </NavbarItem>
-              <NavbarItem>
-                <Button
-                  isIconOnly
-                  variant='light'
-                  size='lg'
-                  radius='full'
-                  as={Link}
-                  href='/cart'
-                  className={`${activePathname === '/cart' ? 'text-[var(--primary-orange-text-color)]' : ''}`}
-                >
-                  <FaShoppingCart className='min-w-5 min-h-5' />
-                </Button>
-              </NavbarItem>
-            </NavbarContent>
-          </Navbar>
-        </>
-      )}
-    </>
+            </NavbarItem>
+          </NavbarContent>
+        </Navbar>
+      </>
+    )
   )
 }
 
